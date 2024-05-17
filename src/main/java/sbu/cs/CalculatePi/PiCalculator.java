9a7bdaf940554392ce43d17ea048d5e73910e0d7
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-17 22:17:59 +0330

    implemented runnable interface

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,19 +1,11 @@
 package sbu.cs.CalculatePi;
 
-public class PiCalculator {
+public class PiCalculator implements Runnable{
 
-    /**
-     * Calculate pi and represent it as a BigDecimal object with the given floating point number (digits after . )
-     * There are several algorithms designed for calculating pi, it's up to you to decide which one to implement.
-     Experiment with different algorithms to find accurate results.
+    @Override
+    public void run() {
 
-     * You must design a multithreaded program to calculate pi. Creating a thread pool is recommended.
-     * Create as many classes and threads as you need.
-     * Your code must pass all of the test cases provided in the test folder.
-
-     * @param floatingPoint the exact number of digits after the floating point
-     * @return pi in string format (the string representation of the BigDecimal object)
-     */
+    }
 
     public String calculate(int floatingPoint)
     {
