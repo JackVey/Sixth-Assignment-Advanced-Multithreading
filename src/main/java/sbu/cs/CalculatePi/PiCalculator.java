f5031bd8ecf29fcea867681acd8450f7ae417d2a
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-17 22:55:55 +0330

    finished programing calculate method

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,13 +1,33 @@
 package sbu.cs.CalculatePi;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PiCalculator{
 
     public String calculate(int floatingPoint)
     {
-
-        return null;
+        BigDecimal pi = BigDecimal.ZERO;
+        double oneMillion = Math.pow(10, 6);
+        ArrayList<PiOperation> piOperations = new ArrayList<>();
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            piOperations.add(new PiOperation(oneMillion * i, oneMillion * (i + 1), floatingPoint));
+            threads.add(new Thread(piOperations.get(i)));
+            threads.get(i).start();
+        }
+        for (int i = 0; i < 10; i++) {
+            try {
+                threads.get(i).join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            pi.add(piOperations.get(i).getPartialPi());
+        }
+        return pi.toString();
     }
 
     public static void main(String[] args) {
