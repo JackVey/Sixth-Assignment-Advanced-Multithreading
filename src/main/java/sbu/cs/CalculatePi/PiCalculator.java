0f0ac2c8563db0e3cf10a3ee235ff12368e4c6fb
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-19 14:23:04 +0330

    changed algorithem from libniz to BBP

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,38 +1,66 @@
 package sbu.cs.CalculatePi;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PiCalculator{
+    static MathContext mc = new MathContext(1100, RoundingMode.HALF_UP);
+    static BigDecimal pi = BigDecimal.ZERO;
+    static BigDecimal one = BigDecimal.ONE;
+    static BigDecimal four = new BigDecimal(4);
+    static BigDecimal two = new BigDecimal(2);
+    public static class PiOperation implements Runnable{
+        private BigDecimal partialPi;
+        double i;
+        PiOperation(double i){
+            this.partialPi = BigDecimal.ZERO;
+            this.i = i;
+        }
+        @Override
+        public void run() {
+            BigDecimal a = four.divide(BigDecimal.valueOf(8L * i + 1), mc);
+            BigDecimal b = two.divide(BigDecimal.valueOf(8L * i + 4), mc);
+            BigDecimal c = one.divide(BigDecimal.valueOf(8L * i + 5), mc);
+            BigDecimal d = one.divide(BigDecimal.valueOf(8L * i + 6), mc);
+            BigDecimal sum = a.subtract(b).subtract(c).subtract(d);
 
+            BigDecimal sixteenPowK = BigDecimal.valueOf(16).pow((int) i);
+            BigDecimal term = sum.divide(sixteenPowK, mc);
+
+            addToPi(term);
+        }
+
+    }
     public String calculate(int floatingPoint)
     {
-        BigDecimal pi = BigDecimal.ZERO;
-        double repetion = Math.pow(10, 8);
-        int numberOfThreads = 20;
-        ArrayList<PiOperation> piOperations = new ArrayList<>();
-        ArrayList<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < numberOfThreads; i++) {
-            piOperations.add(new PiOperation(repetion * i, repetion * (i + 1), floatingPoint));
-            threads.add(new Thread(piOperations.get(i)));
-            threads.get(i).start();
+
+        double repetition = Math.pow(10, 4);
+        int numberOfThreads = 10;
+        ExecutorService threadPool = Executors.newFixedThreadPool(numberOfThreads);
+        for (int i = 0; i < repetition; i++) {
+            PiOperation po = new PiOperation(i);
+            threadPool.execute(po);
         }
-        for (int i = 0; i < numberOfThreads; i++) {
-            try {
-                threads.get(i).join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+
+        threadPool.shutdown();
+
+        try {
+            threadPool.awaitTermination(10000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        for (int i = 0; i < numberOfThreads; i++) {
-            pi = pi.add(piOperations.get(i).getPartialPi());
-        }
+
         return pi.toString().substring(0, floatingPoint + 2);
     }
-
+    public static synchronized void addToPi(BigDecimal value){
+        pi = pi.add(value);
+    }
     public static void main(String[] args) {
         PiCalculator a = new PiCalculator();
-        System.out.println(a.calculate(5));
+        System.out.println(a.calculate(7));
     }
 }
