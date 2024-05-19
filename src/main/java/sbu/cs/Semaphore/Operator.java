6538ad57de31b7c00f0b10afd59e0ac38eef26e6
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-19 20:24:48 +0330

    added semaphore acquire and released statement to Operator class(this is the final commit)

--- src/main/java/sbu/cs/Semaphore/Operator.java
+++ src/main/java/sbu/cs/Semaphore/Operator.java
@@ -4,21 +4,31 @@ import java.util.concurrent.Semaphore;
 
 public class Operator extends Thread {
     private Semaphore semaphore;
+    String name;
     public Operator(String name, Semaphore semaphore) {
-        super(name);
+        this.name = name;
         this.semaphore = semaphore;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++)
-        {
-            Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
-            try {
-                sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        try {
+            semaphore.acquire();
+            System.out.printf("Operator %s accessed to the Resources.%n", name);
+            for (int i = 0; i < 10; i++)
+            {
+                Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+            System.out.printf("Operator %s released the Resources.%n", name);
+            semaphore.release();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
